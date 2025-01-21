fa435914d40074cc1823115b9613332a33a205ce
pocketcorp <pocketcorp@protonmail.com>
2025-01-21 00:07:59 -0800

    completed features section

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,8 +4,8 @@ import { FEATURES } from '@/constants'
 
 const Features = () => {
   return (
-    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container padding-container relative w-full justify-ren">
+    <section className=" flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end flex">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image 
             src='/phone.png'
@@ -26,7 +26,7 @@ const Features = () => {
             />
             <h2 className="bold-40 lg:bold-64">Our Features</h2>
           </div>
-          <ul>
+          <ul className='mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20'>
             {FEATURES.map(feature=>(
               <FeatureItem 
                 key={feature.title}
@@ -51,11 +51,16 @@ type FeatureItem = {
 const FeatureItem = ({title,icon,variant,description}: FeatureItem)=>{
   return(
     <li className="flex w-full flex-1 flex-col items-start">
-      <div className="rounded-full p-4 lg:p-7">
+      <div className="rounded-full p-4 bg-green-50 lg:p-7">
         <Image 
-          src={icon}alt='map'width={28}height={28}
+          src={icon} alt='map' width={28} height={28}
         />
       </div>
+      <h2 className="bold-20 lg:bold-32 mt-5 capitolize">
+        {title}
+      </h2>
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+      {description}</p>
     </li>
   )
 }
